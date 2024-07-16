bef9cb3a0491c4fd3bbdf79f55d5e4ad4e485782
Pavel Us <kirya.sumar@mail.ru>
2024-07-16 18:20:10 +0300

    Добавьте файлы проекта.

---
+++ FuckMe/Models/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace FuckMe.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> TodoItems { get; set; } = null!;
+    }
+}
