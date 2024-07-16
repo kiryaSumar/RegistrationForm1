19a8eea6ad9be0e77148f7972059ebce5b459ff6
Pavel Us <kirya.sumar@mail.ru>
2024-07-16 22:28:17 +0300

    Replaced FuckMe(pls) with RegistrationForm

--- RegistrationForm/RegistrationForm/Program.cs
+++ RegistrationForm/RegistrationForm/Program.cs
@@ -1,15 +1,8 @@
-using Microsoft.EntityFrameworkCore;
-using FuckMe.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<UserContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
