45cb985099f02511a8f97e7b37912e0505d06601
Pavel Us <kirya.sumar@mail.ru>
2024-07-18 16:14:35 +0300

    After about half-our of trying to figure it out

---
+++ RegistrationForm.BLL/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace RegistrationForm.BLL.Services
+{
+    public interface IUserService
+    {// task - read about
+        //Task<User> GetUserByEmail(string email);
+        Task<User> CreateUser(User user);
+    }
+}
+
---
+++ RegistrationForm.BLL/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistrationForm.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        public async Task<User> CreateUser(User user)
+        {
+            // Connection to dal
+
+            return user;
+        }
+    }
+}
