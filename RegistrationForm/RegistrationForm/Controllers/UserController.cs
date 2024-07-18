617119c2e84e13600980be77f4e41101a4c978d1
Pavel Us <kirya.sumar@mail.ru>
2024-07-18 04:03:17 +0300

    added List of users to simulate database

--- RegistrationForm/RegistrationForm/Controllers/UserController.cs
+++ RegistrationForm/RegistrationForm/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RegistrationForm.BLL;
 using RegistrationForm.BLL.Services;
+using RegistrationForm.PL.Contexts;
 
 namespace RegistrationForm.PL.Controllers
 {
@@ -8,6 +9,14 @@ namespace RegistrationForm.PL.Controllers
     [Route("api/v1/[controller]")]//
     public class UserController : Controller
     {
+        static Guid firstUserGuid = new Guid("7247d7c7-e367-459d-9269-eb1f2f1985ae");
+        
+        private static List<User> users = new List<User>(new[] {
+            new User() { Id = firstUserGuid, Name = "Jack",Email = "Jack@mail.ru", Password = "J1"},
+            new User() { Id = Guid.NewGuid(),Name = "Jane",Email = "Jane@mail.ru", Password = "J2"},
+            new User() { Id = Guid.NewGuid(),Name = "Andy",Email = "Andy@mail.ru", Password = "A1"},
+        });
+        private readonly ApplicationContext _context;
         private readonly IUserService _userService;//dependency injection takes place, singletone scope and smth else(уровни изолированности, но не совсем)
         public UserController(IUserService userService)
         {
@@ -15,10 +24,17 @@ namespace RegistrationForm.PL.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<User> GetUser([FromRoute(Name = "id")] Guid Id)
+        // public ActionResult<User> GetUser([FromRoute(Name = "id")] Guid Id)//Полинин Вариант
+        public IActionResult GetUser(Guid Id)//скорее всего, нужно сделать метод асинхронным
         {
-            //var user = await _userService.Users.FindAsync();
-            return Ok(new User());
+            // var user = /*await*/ _context.Users.FindAsync();
+            var user = users.SingleOrDefault(u => u.Id == Id);
+            if (user == null)
+            {
+                 return  NotFound();
+            }
+            //return Ok(new User());
+            return  Ok(user);
         }
 
         [HttpPost]//ActionResult - read about
--- RegistrationForm/RegistrationForm/Program.cs
+++ RegistrationForm/RegistrationForm/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RegistrationForm.BLL.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<IUserService>(options =>
+    //options.UseSqlite(connection));
 
 var app = builder.Build();
 
