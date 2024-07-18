0092500a6c821b6e517c7831bd4951a15828b530
Pavel Us <kirya.sumar@mail.ru>
2024-07-18 17:55:16 +0300

    Added validator(mistake in controller)

--- RegistrationForm/RegistrationForm/Controllers/UserController.cs
+++ RegistrationForm/RegistrationForm/Controllers/UserController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using RegistrationForm.BLL;
 using RegistrationForm.BLL.Services;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace RegistrationForm.PL.Controllers
@@ -9,10 +13,13 @@ namespace RegistrationForm.PL.Controllers
     [Route("api/v1/[controller]")]//
     public class UserController : Controller
     {
+        private UserValidator _validator = new UserValidator;/////////////
         private readonly IUserService _userService;//dependency injection takes place, singletone scope and smth else(уровни изолированности, но не совсем)
-        public UserController(IUserService userService)
+        public UserController(IValidator<User> validator, IUserService userService)
         {
+            _validator = validator;
             _userService = userService;
+
         }
 
         [HttpGet("{id}")]
@@ -23,51 +30,54 @@ namespace RegistrationForm.PL.Controllers
         }
 
         [HttpPost]//ActionResult - read about
-        public ActionResult<User> CreateUser([FromBody] User user)
+        public  ActionResult<User> CreateUser([FromBody] User user)
         {
-            string numbers = "0123456789";
-            bool containsNumber = false;
+
             var saveUser = _userService.CreateUser(user);
-            if (string.IsNullOrEmpty(user.Name))
-            {
-                return BadRequest();
-            }
-            if (string.IsNullOrEmpty(user.Email))
-            {
-                return BadRequest();
-            }
+            ValidationResult result = _validator.Validate(user);
+            //string numbers = "0123456789";
+            //bool containsNumber = false;
 
-            string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
-            Match isMatch = Regex.Match(user.Email, pattern, RegexOptions.IgnoreCase);
+            //if (string.IsNullOrEmpty(user.Name))
+            //{
+            //    return BadRequest();
+            //}
+            //if (string.IsNullOrEmpty(user.Email))
+            //{
+            //    return BadRequest();
+            //}
 
-            if (!isMatch.Success)//email check
-            {
-                return BadRequest();
-            }
-            if (string.IsNullOrEmpty(user.Password))
-            {
-                return BadRequest();
-            }
-            if (user.Password.Length < 8)
-            {
-                return BadRequest();
-            }
-            if (user.Password.ToLower() == user.Password)
-            {
-                return BadRequest();
-            }
-            foreach (var n in numbers)
-            {
-                if (user.Password.Contains(n))
-                { 
-                    containsNumber = true;
-                    break;
-                }
-            }
-            if (!containsNumber)
-            {
-                return BadRequest();
-            }
+            //string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
+            //Match isMatch = Regex.Match(user.Email, pattern, RegexOptions.IgnoreCase);
+
+            //if (!isMatch.Success)//email check
+            //{
+            //    return BadRequest();
+            //}
+            //if (string.IsNullOrEmpty(user.Password))
+            //{
+            //    return BadRequest();
+            //}
+            //if (user.Password.Length < 8)
+            //{
+            //    return BadRequest();
+            //}
+            //if (user.Password.ToLower() == user.Password)
+            //{
+            //    return BadRequest();
+            //}
+            //foreach (var n in numbers)
+            //{
+            //    if (user.Password.Contains(n))
+            //    { 
+            //        containsNumber = true;
+            //        break;
+            //    }
+            //}
+            //if (!containsNumber)
+            //{
+            //    return BadRequest();
+            //}
 
             return Ok(saveUser);
             
